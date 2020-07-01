3d9da4963728b2cd8d89ac231bb63593a5b3a60d
liqiang <liqiang@arhieason.com>
2020-07-01 18:17:36 +0800

    动手学深度学习AlexNet

--- DiveInfoDL/d2lzh.py
+++ DiveInfoDL/d2lzh.py
@@ -8,11 +8,16 @@ import sys
 # ImageAssistant
 
 
-def load_data_fashion_mnist(batch_size):
+def load_data_fashion_mnist(batch_size,resize=None):
 
-    mnist_train = torchvision.datasets.FashionMNIST("../../Datasets/FashionMNIST",train=True,download=True,transform=transforms.ToTensor())
-    mnist_test = torchvision.datasets.FashionMNIST("../../Datasets/FashionMNIST",train=False,download=True,transform=transforms.ToTensor())
+    trans = []
+    if resize:
+        trans.append((transforms.Resize(size=resize)))
+    trans.append(transforms.ToTensor())
+    transform = transforms.Compose(trans)
 
+    mnist_train = torchvision.datasets.FashionMNIST("../../Datasets/FashionMNIST",train=True,download=True,transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST("../../Datasets/FashionMNIST",train=False,download=True,transform=transform)
 
     train_iter = torch.utils.data.DataLoader(mnist_train,batch_size=batch_size,shuffle=True,num_workers=4)
     test_iter = torch.utils.data.DataLoader(mnist_test,batch_size=batch_size,shuffle=True,num_workers=4)
@@ -98,6 +103,33 @@ def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, params=N
             test_acc = evaluate_accuracy(test_iter,net)
 
         print("epoch %d,loss %.4f,train acc %.3f,test_acc %.3f" % (epoch +1,train_l_sum / n ,train_acc_sum / n,test_acc))
+
+def train_ch5(net,train_iter,test_iter,batch_size,optimizer,device,num_epochs):
+    net = net.to(device)
+    print("training on ",device)
+
+    loss = torch.nn.CrossEntropyLoss()
+    batch_count = 0
+    for epoch in range(num_epochs):
+        train_l_sum,train_acc_sum,n,start = 0.0,0.0,0,time.time()
+
+        for x,y in train_iter:
+            x = x.to(device)
+            y = y.to(device)
+            y_hat = net(x)
+            l = loss(y_hat,y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+
+            n += y.shape[0]
+            batch_count += 1
+        test_acc = evaluate_accuracy(test_iter,net)
+
+        print('epoch %d, loss %.4f, train acc %.3f,test acc %.3f,time %.1f' %(epoch + 1,train_l_sum / batch_count,train_acc_sum / n ,test_acc,time.time()-start))
 
 def semilogy(x_vals,y_vals,x_label,y_label,x2_vals=None,y2_vals=None,legend=None):
     plt.xlabel(x_label)

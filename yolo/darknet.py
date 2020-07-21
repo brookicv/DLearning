428cdee6c40d127aede6cd5f6332046160f46f04
liqiang <liqiang@arhieason.com>
2020-07-21 18:18:37 +0800

    yolo nms

--- yolo/darknet.py
+++ yolo/darknet.py
@@ -63,3 +63,82 @@ class Darknet(nn.Module):
             outputs[i] = x
 
         return detection
+
+    def load_weights(self, weightfile):
+        
+        fp = open(weightfile, "rb")
+        
+        # 前5个值表示header
+        # 1. Major version numbers
+        # 2. Minor version numbers
+        # 3. Subversion numbers
+        # 4. 5. Image seen by the network(during training)
+
+        header = np.fromfile(fp, dtype=np.int32, count=5)
+        self.header = torch.from_numpy(header)
+        self.seen = self.header[3]
+        
+        # 余下部分为权重，权重以float32的形式保存
+        weights = np.fromfile(fp, dtype=np.float32)
+        ptr = 0
+        for i in range(len(self.module_list)):
+            
+            module_type = self.blocks[i + 1]["type"]
+            if module_type == "convolutional":
+                model = self.module_list[i]
+                try:
+                    batch_normalize = int(self.blocks[i + 1]["batch_normalize"])
+                except:
+                    batch_normalize = 0
+                conv = model[0]
+
+                if batch_normalize:
+                    bn = model[1] # batch normalization layer
+
+                    num_bn_biases = bn.bias.numel()  # 得到权重的个数(bn layer)
+                    
+                    # load weights
+                    bn_biases = torch.from_numpy(weights[ptr:ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_weights = torch.from_numpy(weights[ptr:ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_mean = torch.from_numpy(weights[ptr:ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_var = torch.from_numpy(weights[ptr:ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_biases = bn_biases.view_as(bn.bias.data)
+                    bn_weights = bn_weights.view_as(bn.weight.data)
+                    bn_running_mean = bn_running_mean.view_as(bn.running_mean)
+                    bn_running_var = bn_running_var.view_as(bn.running_var)
+
+                    # copy data to model
+                    bn.bias.data.copy_(bn_biases)
+                    bn.weight.data.copy_(bn_weights)
+                    bn.running_mean.copy_(bn_running_mean)
+                    bn.running_var.copy_(bn_running_var)
+
+                else:
+
+                    # bias个数计算
+                    num_biases = conv.bias.numel()
+
+                    # load weights
+                    conv_biases = torch.from_numpy(weights[ptr:ptr + num_biases])
+                    ptr += num_biases
+
+                    conv_biases = conv_biases.view_as(conv.bias.data)
+
+                    conv.bias.data.copy_(conv_biases)
+
+                num_weights = conv.weight.numel()
+
+                conv_weights = torch.from_numpy(weights[ptr:ptr + num_weights])
+                ptr += num_weights
+
+                conv_weights = conv_weights.view_as(conv.weight.data)
+                conv.weight.data.copy_(conv_weights)
+
--- yolo/utils.py
+++ yolo/utils.py
@@ -265,10 +265,140 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, device):
 
     return prediction
 
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+    return tensor_res
+
+def bbox_iou(box1, box2):
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+    
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.min(b1_x2, b2_x2)
+    inter_rect_y2 = torch.min(b1_y2, b2_y2)
+
+    # intersection area 
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+    
+    b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x2 + 1) * (b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area)
+
+    return iou
+
+def write_results(prediction, confidence, num_classes, nms_conf=0.4):
+    
+    conf_mask = (prediction[:,:, 4] > confidence).float().unsqueeze(2)
+    prediction = prediction * conf_mask
+
+    box_corner = prediction.new(prediction.shape)
+    box_corner[:,:, 0] = prediction[:,:, 0] - prediction[:,:, 2] / 2
+    box_corner[:,:, 1] = prediction[:,:, 1] - prediction[:,:, 3] / 2
+    box_corner[:,:, 2] = prediction[:,:, 0] + prediction[:,:, 2] / 2
+    box_corner[:,:, 3] = prediction[:,:, 1] + prediction[:,:, 3] / 2
+    prediction[:,:,:4] = box_corner[:,:,:4]
+
+    batch_size = prediction.shape[0]
+
+    write = False
+
+    for ind in range(batch_size):
+        image_pred = prediction[ind]
+
+        max_conf, max_conf_score = torch.max(image_pred[:, 5:5 + num_classes], 1) # 取到各个类别对应的概率,
+        max_conf = max_conf.float().unsqueeze(1) # max_conf最大值
+        max_conf_score = max_conf_score.float().unsqueeze(1) #  max_conf_score最大值的索引,类别的index
+        seq = (image_pred[:,:5], max_conf, max_conf_score)   # x1,x2,y1,y2,confidence,max_conf,max_conf_score
+        image_pred = torch.cat(seq, 1) #合并到一起
+        
+        non_zero_ind = torch.nonzero(image_pred[:, 4]) # 过滤掉confidence小于置信度阈值的bbox
+        try:
+            image_pred_ = image_pred[non_zero_ind.squeeze(),:].view(-1, 7) # 取出置信度大于confidence的bbox
+        except:
+            continue
+
+        if image_pred_.shape[0] == 0: # 没有检测到物体
+            continue
+        
+        # 找出图片中的所有类别，也就是过滤掉重复出现的类别
+        img_classes = unique(image_pred_[:, -1])  # 取得是概率最大值的索引max_conf_score,也就是类别的index
+        
+        for cls in img_classes:
+            cls_mask = image_pred_ * (image_pred_[:, -1] == cls).float().unsqueeze(1)  # 提取出其中的某个类别
+            class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()  # 类别概率不为0的bbox
+            
+            image_pred_class = image_pred_[class_mask_ind].view(-1, 7)  # 取出预测类别为cls的所有bbox
+
+            conf_sort_index = torch.sort(image_pred_class[:, 4], descending=True)[1] #根据置信度confidence对bbox进行排序
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)  # bbox的个数
+            
+            for i in range(idx):
+                try:
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i + 1:])  # 计算任一边框i与i+1之后的所有边框的iou
+                except ValueError:
+                    break
+                except IndexError:
+                    break
+                iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                image_pred_class[i + 1:] *= iou_mask  # iou大于阈值的边框,设置为0
+                
+                non_zero_ind = torch.nonzero(image_pred_class[:, 4]).squeeze()
+                image_pred_class = image_pred_class[non_zero_ind].view(-1, 7)
+
+            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
+            seq = batch_ind, image_pred_class
+            if not write:
+                output = torch.cat(seq, 1)
+                write = True
+            else:
+                out = torch.cat(seq, 1)
+                output = torch.cat((output, out))
+                
+    return output
+
+
+def load_classes(namesfile):
+    fp = open(namesfile, "r")
+    names = fp.read().split("\n")[:-1]
+    return names
+
+def letterbox_image(img, inp_dim):
+    '''resize image with unchanged aspect ratio using padding'''
+    img_w, img_h = img.shape[1], img.shape[0]
+    w, h = inp_dim
+    new_w = int(img_w * min(w/img_w, h/img_h))
+    new_h = int(img_h * min(w/img_w, h/img_h))
+    resized_image = cv2.resize(img, (new_w,new_h), interpolation = cv2.INTER_CUBIC)
+    
+    canvas = np.full((inp_dim[1], inp_dim[0], 3), 128)
+
+    canvas[(h-new_h)//2:(h-new_h)//2 + new_h,(w-new_w)//2:(w-new_w)//2 + new_w,  :] = resized_image
+    
+    return canvas
+
+def prep_image(img, inp_dim):
+    """
+    opencv以numpy数组的形式加载图像，以BGR为颜色通道的顺序。
+    PyTorch的图像输入格式为batch x channel x height x width，通道顺序为RGB
+    """
+    img = letterbox_image(img, inp_dim)
+    img = img[:,:,::-1].transpose((2, 0, 1)).copy()
+    img = torch.from_numpy(img).float().div(255.0).unsqueeze(0)
+
+    return img
+
 def get_test_input():
 
     img = cv2.imread("person.jpg")
-    img = cv2.resize(img, (416, 416))
+    img = cv2.resize(img, (608, 608))
     img = img[:,:,::-1].transpose(2, 0, 1)
     img = img[np.newaxis,:,:,:] / 255.0
     img = torch.from_numpy(img).float()
